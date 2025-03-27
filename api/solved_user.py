e635bc686185e22684c1580f7d4908789b7a5cb1
YJHeo01 <sk14cj@inu.ac.kr>
2025-03-27 13:38:46 +0900

    난이도 기여 추가

--- api/solved_user.py
+++ api/solved_user.py
@@ -1,2 +1,14 @@
+import requests
+
 def solved_user_data(username):
-    return [0]
+    url = "https://solved.ac/api/v3/user/show"
+    
+    querystring = {"handle":username}
+    headers = {
+        "x-solvedac-language": "",
+        "Accept": "application/json"
+    }
+
+    response = requests.get(url, headers=headers,params=querystring)
+    
+    return [response.json()['voteCount']]
