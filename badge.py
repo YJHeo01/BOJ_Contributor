7b921c571286106a41d8b0a53fc45bb2055b7ddf
YJHeo01 <sk14cj@inu.ac.kr>
2025-03-27 12:56:43 +0900

    ㅇㅇ

--- badge.py
+++ badge.py
@@ -7,6 +7,6 @@ def create_svg(data):
     dwg.add(dwg.text("문제 제작   :   "+str(data[1]), insert=(10, 60), fill="black", font_size="14px"))
     dwg.add(dwg.text("문제 검수   :   "+str(data[2]), insert=(200, 60), fill="black", font_size="14px"))
     dwg.add(dwg.text("BOJ Contributor", insert=(10, 100), fill="black", font_size="14px"))
-    #dwg.add(dwg.text("오류 제보: " +str(data[4]), insert=(10, 75), fill="black", font_size="14px"))
-    dwg.add(dwg.text("난이도 기여   :   "+str(data[3]), insert=(200, 100), fill="black", font_size="14px"))
+    #dwg.add(dwg.text("오류 제보: " +str(data[3]), insert=(10, 75), fill="black", font_size="14px"))
+    dwg.add(dwg.text("난이도 기여   :   "+str(data[4]), insert=(200, 100), fill="black", font_size="14px"))
     return dwg.tostring()
