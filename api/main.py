1f2a9fcea1da67bc706d36029e71696e5f46ab83
YJHeo01 <sk14cj@inu.ac.kr>
2025-03-28 10:33:40 +0900

    파일명, README 수정

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 import sys
-from api.boj_user import boj_user_data
-from api.solved_user import solved_user_data
+from api.boj_user_page import boj_user_data
+from api.solved_user_page import solved_user_data
 
 def main(username):
     user_data = boj_user_data(username) + solved_user_data(username)
