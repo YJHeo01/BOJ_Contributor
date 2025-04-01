ebf98ca614436f68dfc48893418fe7689ec13374
YJHeo01 <sk14cj@inu.ac.kr>
2025-04-01 12:55:22 +0900

    gitignore 수정

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ def hello(username):
     image, hash = badge.create_svg(user_data)
     if request.args.get('v') != hash:
         return redirect(url_for('show_user_profile', username=username, v=hash))
-    return Response(image,mimetype='image/svg+xml')
+    response = Response(image,mimetype='image/svg+xml')
+    response.headers['Cache-Control'] = 'public, max-age=3600'
+    return response
 
 @app.route('/user/<username>')
 def show_user_profile(username):
@@ -18,7 +20,9 @@ def show_user_profile(username):
     image, hash = badge.create_svg(user_data)
     if request.args.get('v') != hash:
         return redirect(url_for('show_user_profile', username=username, v=hash))
-    return Response(image,mimetype='image/svg+xml')
+    response = Response(image,mimetype='image/svg+xml')
+    response.headers['Cache-Control'] = 'public, max-age=3600'
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
